4c17a9485edddde2a95b9b9c9f7be653445d5f15
Vladimir Azpeitia <vazpeitiah@gmail.com>
2025-03-12 07:19:52 -0600

    test: add account update functionality to AccountForm tests

--- test/pages/accounts/account-form/account-form.test.tsx
+++ test/pages/accounts/account-form/account-form.test.tsx
@@ -7,12 +7,14 @@ import { AccountForm } from '@/pages'
 import { TestWrapper } from '../../../setup/wrapper'
 import { server } from '../../../setup/server'
 import { ROUTES } from '@/utils/const'
+import { Account } from '@/models/account'
 
 const mockNavigate = vi.fn()
 
 const mocks = vi.hoisted(() => {
   return {
     mockSuccess: vi.fn(),
+    mockState: null as { account: Account } | null,
   }
 })
 
@@ -31,6 +33,9 @@ vi.mock('react-router', async (importOriginal) => {
   return {
     ...actual,
     useNavigate: () => mockNavigate,
+    useLocation: () => ({
+      state: mocks.mockState,
+    }),
   }
 })
 
@@ -49,9 +54,12 @@ describe('AccountForm', () => {
   })
 
   it('should create an account correctly', async () => {
+    const requestSpy = vi.fn()
     const user = userEvent.setup()
     server.use(
-      http.post('/accounts', () => {
+      http.post('/accounts', async ({ request }) => {
+        const body = await request.json()
+        requestSpy(body)
         return HttpResponse.json({
           id: 'cm7o6w9xm00014aa22bfoombt',
           name: 'New Account',
@@ -73,9 +81,65 @@ describe('AccountForm', () => {
     await user.click(options[1])
     await user.click(screen.getByText('buttons.save'))
 
+    expect(requestSpy).toHaveBeenCalledWith({
+      name: 'New Account',
+      type: 'cash',
+    })
     expect(mockNavigate).toHaveBeenCalledWith(ROUTES.accounts.root)
     expect(mocks.mockSuccess).toHaveBeenCalledWith(
       'Account created successfully',
     )
+  })
+
+  it('should update an account correctly', async () => {
+    const requestSpy = vi.fn()
+    mocks.mockState = {
+      account: {
+        id: 'cm7o6w9xm00014aa22bfoombt',
+        name: 'Account',
+        type: 'cash',
+      },
+    }
+
+    const user = userEvent.setup()
+    server.use(
+      http.put('/accounts/cm7o6w9xm00014aa22bfoombt', async ({ request }) => {
+        const body = await request.json()
+        requestSpy(body)
+        return HttpResponse.json({
+          id: 'cm7o6w9xm00014aa22bfoombt',
+          name: 'Updated Account',
+          type: 'credit_card',
+        })
+      }),
+    )
+
+    render(
+      <TestWrapper>
+        <AccountForm />
+      </TestWrapper>,
+    )
+
+    await user.clear(screen.getByLabelText('accounts.form.name'))
+    await user.type(
+      screen.getByLabelText('accounts.form.name'),
+      'Updated Account',
+    )
+    await user.click(screen.getByLabelText('accounts.form.type'))
+    const options = await screen.findAllByText(
+      'catalogs.accountTypes.credit_card',
+    )
+    /* the options[0] is hidden */
+    await user.click(options[1])
+    await user.click(screen.getByText('buttons.save'))
+
+    expect(requestSpy).toHaveBeenCalledWith({
+      name: 'Updated Account',
+      type: 'credit_card',
+    })
+    expect(mocks.mockSuccess).toHaveBeenCalledWith(
+      'Account updated successfully',
+    )
+    expect(mockNavigate).toHaveBeenCalledWith(ROUTES.accounts.root)
   })
 })
